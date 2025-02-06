e5265d4a1dce31928794da536c61988524037ca2
Ostap Konstantinov <ostap.konstantinov@yandex.ru>
2025-02-06 05:54:25 +0300

    Add files

---
+++ level_1/22/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	// Инициализация больших чисел a и b
+	a := new(big.Int)
+	b := new(big.Int)
+
+	// Установка значений a и b (больше 2^20)
+	a.SetString("123456789012345678901234567890", 10) // Пример значения для a
+	b.SetString("987654321098765432109876543210", 10) // Пример значения для b
+
+	// Сложение
+	sum := new(big.Int)
+	sum.Add(a, b)
+	fmt.Printf("Сумма: %s\n", sum.String())
+
+	// Вычитание
+	diff := new(big.Int)
+	diff.Sub(a, b)
+	fmt.Printf("Разность: %s\n", diff.String())
+
+	// Умножение
+	prod := new(big.Int)
+	prod.Mul(a, b)
+	fmt.Printf("Произведение: %s\n", prod.String())
+
+	// Деление
+	quotient := new(big.Int)
+	quotient.Div(a, b)
+	fmt.Printf("Частное: %s\n", quotient.String())
+}
---
+++ level_1/23/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Сохраняем порядок элементов
+func remOneElement(s []int, i int) []int {
+	return append(s[:i], s[i+1:]...)
+}
+
+// Игнорируем порядок элементов
+func remOneElementV2(s []int, i int) []int {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(remOneElement(a, 3))
+
+	b := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(remOneElementV2(b, 3))
+}
